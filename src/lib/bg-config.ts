92cc2c07764a380d6170a7728b636fb16ad724fc
bRight36691 <bright.veerawat@gmail.com>
2025-11-03 14:48:40 +0700

    preload

--- src/lib/bg-config.ts
+++ src/lib/bg-config.ts
@@ -1,29 +1,165 @@
 export const backgroundMapConfig = {
-
+  "0-1" : {
+    image : "",
+    imagePreload: ["/img/scene1-1.webp"],
+    
+  },
+  "1-1" : {
+    image : "",
+    imagePreload: ["/img/scene1-3.webp"],
+    
+  },
+  "1-2" : {
+    image : "",
+    imagePreload: ["/img/scene1-3.webp"],
+    
+  },
+  "1-3" : {
+    image : "",
+    imagePreload: ["/img/scene1-4.webp"],
+    
+  },
+  "1-4" : {
+    image : "",
+    imagePreload: ["/img/scene1-8.webp"],
+    
+  },
     "r-1": {
       image: "/img/scene1-8.webp",
-      imagePreload: [],
+      imagePreload: ["/img/scene1-8.webp"],
     },
     "r-2": {
         image: "/img/scene1-8.webp",
-        imagePreload: [],
+        imagePreload: ["/img/scene1-4.webp"],
+      },
+    "1-8": {
+        image: "",
+        imagePreload: ["/img/Q1.webp"],
+      },
+    "Q1": {
+        image: "",
+        imagePreload: ["/img/Q2.webp"],
+      },
+    "Q2": {
+        image: "",
+        imagePreload: ["/img/Q3.webp"],
+      },
+    "Q3": {
+        image: "",
+        imagePreload: ["/img/scene2-1.webp"],
+      },
+    "2-1": {
+        image: "",
+        imagePreload: ["/img/scene2-2.webp"],
+      },
+    "2-2": {
+        image: "",
+        imagePreload: ["/img/scene3-1.webp"],
+      },
+    "3-1": {
+        image: "",
+        imagePreload: ["/img/scene3-2.webp"],
+      },
+    "3-2": {
+        image: "",
+        imagePreload: ["/img/scene3-3.webp"],
       },
     "3-3": {
         image: "/img/scene3-3.webp",
-        imagePreload: [],
+        imagePreload: ["/img/Q4.webp"],
+      },
+    "Q4": {
+        image: "/img/Q4.webp",
+        imagePreload: ["/img/scene3-4.webp"],
+      },
+    "34-4": {
+        image: "",
+        imagePreload: ["/img/scene4-1.webp"],
+      },
+    "4-1": {
+        image: "",
+        imagePreload: ["/img/Q5.webp"],
+      },
+    "Q5": {
+        image: "",
+        imagePreload: ["/img/scene4-2.webp"],
+      },
+    "4-2": {
+        image: "",
+        imagePreload: ["/img/Q6.webp"],
+      },
+    "Q6": {
+        image: "",
+        imagePreload: ["/img/Q7.webp"],
+      },
+    "Q7": {
+        image: "",
+        imagePreload: ["/img/scene4-3.webp"],
+      },
+    "41-3": {
+        image: "",
+        imagePreload: ["/img/Q8.webp"],
+      },
+    "Q8": {
+        image: "",
+        imagePreload: ["/img/scene5-1.webp"],
+      },
+    "41-4": {
+        image: "",
+        imagePreload: ["/img/Q9_ear.webp","/img/Q9_smoke.webp"],
+      },
+    "Q9": {
+        image: "",
+        imagePreload: ["/img/Q10.webp"],
+      },
+    "Q10": {
+        image: "",
+        imagePreload: ["/img/scene5-2.webp"],
+      },
+    "5-2": {
+        image: "",
+        imagePreload: ["/img/scene5-3-1.webp",
+                      "/img/scene5-3-2.webp",
+                      "/img/scene5-3-3.webp",
+                      "/img/scene5-3-4.webp"],
+      },
+    "5-3": {
+        image: "",
+        imagePreload: ["/img/Q11.webp"],
+      },
+    "Q11": {
+        image: "",
+        imagePreload: ["/img/scene7-1.webp"],
       },
     "7-1": {
         image: "/img/scene7-1.webp",
         imagePreload: [],
       },
+    "72-2": {
+        image: "",
+        imagePreload: ["/img/Q12.webp"],
+      },
+    "Q12": {
+        image: "",
+        imagePreload: ["/img/scene8-1.webp"],
+      },
+    "8-1": {
+        image: "",
+        imagePreload: ["/img/scene9-1.webp",
+                      "/img/scene9-1-firework.webp",
+                      "/img/scene9-1-shadow.webp"],
+      },
+
     "9-1": {
         image: "/img/scene9-1.webp",
-        imagePreload: [],
+        imagePreload: ["/img/scene10-1.webp"],
       },
-    "Q4": {
-        image: "/img/Q4.webp",
-        imagePreload: [],
-    },
+    "9-2": {
+        image: "/img/scene9-1.webp",
+        imagePreload: ["/img/scene10-1.webp"],
+      },
+
+
     
   } as const;
      
